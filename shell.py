79a3f2a62ecc7133e1772b8e419f0c1619a3f543
Corey Mckeel <coreyt.mckeel@gmail.com>
2021-11-27 16:59:14 -0500

    cleaner print statements

--- shell.py
+++ shell.py
@@ -9,9 +9,10 @@ while True:
 
     if error: print(error.as_string())
     elif result:
-        if len(result.elements) == 1:
+        if len(result.elements) <= 1:
             print(repr(result.elements[0]))
-        print(repr(result))
+        else:
+            print(repr(result))
 
 # import comparison
 # while True:
